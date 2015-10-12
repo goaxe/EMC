0c0e08b539afcf82c70626d3dfeb7ea4d97baa60
Jian Huang <huangjian1993@gmail.com>
2015-10-12 17:43:30 +0800

    finish redis-cache, requester

---
+++ 20151010/redis-cache/redis_transform.py
@@ -0,0 +1,46 @@
+import string
+class Log:
+    def __init__(self):
+        self.startTime = 0
+        self.endTime = 0
+        self.memUsed = 0
+
+for i in range(1, 4):
+    file = open("redis-master" + str(i) + ".log")
+    for j in range(1, 25):
+        file.readline()
+
+    logs = []
+    count = 0
+    lineNum = 0
+    while 1:
+        log = Log()
+        line = file.readline()
+        if not line:
+            break
+        strs = line.split(" ")
+        log.startTime = " ".join(strs[1: 4])
+        file.readline()
+        if not line:
+            break
+        file.readline()
+        if not line:
+            break
+        line = file.readline()
+        if not line:
+            break
+        strs = line.split(" ")
+        log.memUsed = strs[6] + "MB"
+        line = file.readline()
+        if not line:
+            break
+        strs = line.split(" ")
+        log.endTime = " ".join(strs[1: 4])
+        logs.append(log)
+    file.close()
+    f = open("redis-master" + str(i) + ".csv", 'w')
+    f.write("star_time,end_time,mem_used,\n")
+    for log in logs:
+        line = "%s,%s,%s\n" % (log.startTime, log.endTime, log.memUsed)
+        f.write(line)
+    f.close()
