8b203f13383fd8e80c402391e745e886fb57ce19
axe <huangjian1993@gmail.com>
2016-03-23 08:16:13 +0800

    modify js

--- Smoothie/app.py
+++ Smoothie/app.py
@@ -3,6 +3,7 @@ from flask.ext.redis import FlaskRedis
 import random
 
 REDIS_URL = 'redis://localhost:6379/0'
+IS_LOCAL = True
 
 app = Flask(__name__)
 redis_store = FlaskRedis(app)
@@ -17,8 +18,11 @@ def create_data():
     for i in range(0, 10):
         for port in ports:
             nums = [str(random.uniform(10, 20)), str(random.uniform(10, 30))]
-            print nums
-            redis_store.rpush('k' + port, ','.join(nums))
+            if IS_LOCAL:
+                redis_store.rpush('k' + port, ','.join(nums))
+            else:
+                redis_store.rpush(port, ','.join(nums))
+        redis_store.rpush('entropy', str(random.uniform(10, 20)))
     return "create data success"
 
 
@@ -27,8 +31,12 @@ def data():
     ports = ['80', '25', '443', '53', '22', '21', '67', '110', '143', '137', '138', '139', '1024-', '1024+']
     size = []
     count = []
+    entropy = []
     for port in ports:
-        tmp = redis_store.lpop('k' + port)
+        if IS_LOCAL:
+            tmp = redis_store.lpop('k' + port)
+        else:
+            tmp = redis_store.lpop(port)
         if tmp is None:
             size.append(0)
             count.append(0)
@@ -36,9 +44,13 @@ def data():
             tmp = tmp.split(',')
             size.append(tmp[0])
             count.append(tmp[1])
+    tmp = redis_store.lpop('entropy')
+    if tmp is None:
+        tmp = 0
     da = {}
     da['size'] = size
     da['count'] = count
+    da['entropy'] = tmp
     return jsonify(**da)
 
 if __name__ == '__main__':
